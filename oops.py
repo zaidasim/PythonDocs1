541c576476302027c8c01fdcf8fed0e7ad3ae3a8
Muhammad <zaidasim94@gmail.com>
2024-07-24 16:07:52 +0500

    Added oops.py

--- oops.py
+++ oops.py
@@ -27,7 +27,7 @@ class ClassName:
     def __str__(self):
         return f"ClassName with data: {self.data}"
 
-class D_from_P(ClassName):
+class D_from_P(ClassName):#inheritence
     def __init__(self, value):
 
         self.data = value  # Overwrite or set additional data if needed
@@ -52,3 +52,64 @@ class D_from_P(ClassName):
 P = D_from_P(5)
 print(P.__str__())  # Use the __str__ method
 P.to_decorate()     # Call the decorated method
+
+
+#Abstraction is defining a base abstract class  and a virtual function, which defines an informal blue print to follow
+class Shape:
+  
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+circle = Circle(5)
+square = Square(4)
+
+print(circle.area())  
+print(square.area())  
+#Polymorphism is a way in which  the compiler automatically calls derived functions that are of same name
+class Bird:
+    def fly(self):
+        print("Bird does")
+
+class Aeroplane:
+    def fly(self):
+        print("Aeroplane does")
+
+def flies(can_fly):
+    can_fly.fly()
+
+bird = Bird()
+plane = Aeroplane()
+
+flies(bird)  
+flies(plane) 
+#Encapsulation is a phenomenon in which class's members are made private with (__) at start making it fixed with in the class and only the same class members can access it
+class Car:
+    def __init__(self, making, wheels):
+        self.making = making       
+        self.__wheels = wheels   # here wheels is private and can only be accessed by a private member function and not  by it self
+
+    def get_wheels(self):
+        return self.__wheels
+
+    def set_wheels(self, wheels):
+        self.__wheels = wheels
+
+meri_car = Car("Mehran", "2 wheeler")
+print(meri_car.making)            
+print(meri_car.get_wheels())  
+#   print(meri_car.__wheels) ''''It is not allowed and will result in an error
+print(meri_car._Car__wheels)
