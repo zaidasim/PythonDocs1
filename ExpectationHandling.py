6ce849d0d1e7b643efd1fd8372c52d27386f0c72
Muhammad <zaidasim94@gmail.com>
2024-07-24 13:44:27 +0500

    Added ExpectationHandling.py

--- ExpectationHandling.py
+++ ExpectationHandling.py
@@ -1,1 +1,15 @@
 
+#To handle different types of errors we use try and expect
+while True:
+    try:
+        x = int("33f")
+        break
+    except ValueError:
+        print("Try again...")
+        break
+#The raise statement allows the programmer to force a specified exception to occur
+try:
+   print("try")
+finally:
+    print('finally')
+#the finally will run before try weather or not it becomes true 
